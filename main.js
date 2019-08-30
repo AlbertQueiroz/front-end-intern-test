c5320c994153053feac387547702c612bd2f0e72
Albert Rayneer <rayneerqueiroz@gmail.com>
2019-08-30 11:08:37 -0300

    Added js constants
    email and messages (Value/Length) constants added

--- main.js
+++ main.js
@@ -15,11 +15,15 @@
   contactForm.addEventListener('submit', function() {
     // A propriedade "value" contém o valor atual de um input ou textarea
     const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
 
     // A propriedade "length" contém o tamanho (em caracteres) de uma
     // uma string. Toda string contém essa propriedade.
     const nameLength = nameValue.length;
-
+    const emailLength = emailValue.length;
+    const messageLength = messageValue.length;
+    
     // "window" é um objeto global que representa a janela (ou aba) do
     // navegador que está executando o código do seu site. O método
     // "alert" simplesmente mostra um aviso para o usuário contendo a
